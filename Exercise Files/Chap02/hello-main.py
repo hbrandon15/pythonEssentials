59b0f65da3a18cba0f08466cb57bfc25aebfac9e
Brandon Hernandez <hernandezbrandon17@gmail.com>
2020-06-03 11:30:55 -0500

    Updated files.

--- Exercise Files/Chap02/hello-main.py
+++ Exercise Files/Chap02/hello-main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
-
+# This is functional the same as hello-version.py
 import platform
+
 
 def main():
     message()
 
+
 def message():
     print('This is python version {}'.format(platform.python_version()))
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__': main()  # this causes the interpreter to read the entire script before executing
--- Exercise Files/Chap02/hello-version.py
+++ Exercise Files/Chap02/hello-version.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 # This is a shebang line. This is primarily used for UNIX based systems.
 # Copyright 2009-2017 BHG http://bw.org/
 
-import platform
+import platform  # the platform module is used to display the version of python you are using.
 
 print('This is python version {}'.format(platform.python_version()))
